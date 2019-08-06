8a4e633237e084e107d1cf37e4363b9cf82764e4
Alex Loney <loney.alex@gmail.com>
2019-08-05 17:58:39 -0700

    Adding a word list and attempt to parse message to make a more human readable format

--- index.js
+++ index.js
@@ -2,8 +2,38 @@ const Discord = require('discord.js');
 const token = require('./token.json');
 const botconfig = require('./botconfig.json');
 const codebook = require('./codebook.json');
+const fs = require('fs');
 
 const client = new Discord.Client();
+
+let dict = fs.readFileSync('./words_alpha.txt').toString().toUpperCase().split('\r\n');
+dict.sort((a, b) => b.length - a.length);
+
+function isAtPos(a, b, pos)
+{
+    if (a.length - pos < b.length) return false;
+
+    if (a.indexOf(b) == pos) return true;
+
+    return false;
+}
+
+function decode(str, pos, arr)
+{
+    dict.forEach(word => {
+        if (pos === str.length) return;
+        if (arr.join('').length === str.length) return;
+        if (isAtPos(str, word, pos)) {
+            pos += word.length;
+            arr.push(word);
+            decode(str, pos, arr);
+
+            if (arr.join('').length === str.length) return;
+
+            arr.pop();
+        }
+    });
+}
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -74,10 +104,20 @@ client.on('message', message => {
             pos += 1;
         });
         
-        const embed = new Discord.RichEmbed()
+        let desc = decoded.join('');
+        let embed = new Discord.RichEmbed()
             .setTitle('Decoded Message')
-            .setColor(0x00FF00)
-            .setDescription(decoded.join(''));
+            .setColor(0x00FF00);
+
+
+        // let words = [];
+        // pos = 0;
+        // decode(decoded.join(''), pos, words);
+        // if (words.join('').length === decoded.length) {
+        //     desc += '\nPossible meaning: ' + words.join(' ');
+        // }
+
+        embed.setDescription(desc);
         
         message.channel.send(embed);
     }
